5eade6cacbb72791bfbd43bd202f7d3059a0e5fa
LadyKerr <kadiekerr@gmail.com>
2019-05-09 21:58:30 -0400

    removed console.logs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,14 +6,12 @@ class TabLink {
     
     // Get the custom data attribute on the Link
     this.data = this.element.dataset.tab;
-    console.log(this.data);
+    
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
-    console.log(this.itemElement);
     
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
-    console.log(this.tabItem);
     
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener('click', () => this.select());
